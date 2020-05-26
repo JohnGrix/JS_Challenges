88d0e6bc7ded3d7ffd9c75805189bc5ea3ca175d
Juan Groh <juangroh@yahoo.com>
2020-05-26 13:18:38 +0200

    [Email-Sending] JS > Email validation improved & error detection solved

--- Simulate-Email-Sending/js/app.js
+++ Simulate-Email-Sending/js/app.js
@@ -9,7 +9,7 @@ const enviarBtn = document.getElementById("enviar");
 // Event Listeners
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
     // Ejecutado al cargar el contenido del DOM > Deshabilita enviarBtn
     document.addEventListener("DOMContentLoaded", inicioApp);
 
@@ -23,67 +23,81 @@ function eventListeners(){
 
 // Funciones
 // Funcion ejecutada al cargar la pagina > Deshabilita enviarBtn
-function inicioApp(){
+function inicioApp() {
     // Deshabilitar boton Enviar (enviarBtn)
     enviarBtn.disabled = true;
 }
 
 // Funcion que valida que el campo tenga algo escrito
-function validarCampo(){
+function validarCampo() {
     // console.log("Dentro del INPUT"); - Cheaqueando que el EventListener funcione
-    
+
     // Validar la longitud del texto y que este no este vacio
     validarLongitud(this);
 
     // Validar la direccion de email
     // console.log(this.type); - Ver que campo es el que llama a la funcion
     // Si la funcion es llamada por el campo email, validar que contenga un @ en su texto
-    if(this.type === "email"){
+    if (this.type === "email") {
         validarEmail(this);
     }
-
+    
     // Reviso el documento buscando por algun elemento que tenga la clase .error. Si no encuentro ninguno, el Array permanece vacio
-    let errores = document.querySelectorAll("error");
-
+    let errores = document.querySelectorAll(".error");
+    
     // Si ninguno de los campos del formulario esta vacio
-    if(email.value != "" && asunto.value != "" && mensaje.value != "") {
+    if (email.value != "" && asunto.value != "" && mensaje.value != "") {
         // Si no hay ningun campo que contenga .error  
-        if(errores.length === 0){
+        if (errores.length === 0) {
             // Habilitar el boton de Enviar
             enviarBtn.disabled = false;
+        } else {
+            // Deshabilitar el boton de Enviar
+            enviarBtn.disabled = true;
         }
+    } else {
+        // Deshabilitar el boton de Enviar
+        enviarBtn.disabled = true;
     }
 }
 
 // Validar que los campos del formulario no esten vacios
-function validarLongitud(campo){
+function validarLongitud(campo) {
     // console.log(campo); - Chequeando que campo invoca la funcion
     // console.log(campo.value.length); - Chequeo si el campo tiene algo escrito
 
     // Si la cantidad de caracteres en el campo del formulario es mayor a 0 (no esta vacio)
-    if(campo.value.length > 0){
-        // Cambiar el color del borde inferior a verde
-        campo.style.borderBottomColor = "green";
-        // Remover la clase "error" del elemento
-        campo.classList.remove("error");
+    if (campo.value.length > 0) {
+        aprobarCampo(true, campo);
     } else {
-        // Cambiar el color del borde inferior a rojo
-        campo.style.borderBottomColor = "red";
-        // Remover la clase "error" del elemento
-        campo.classList.add("error");
+        aprobarCampo(false, campo);
     }
 }
 
 // Validar la direccion email ingresada en el formulario
-function validarEmail(campo){
+function validarEmail(campo) {
     const email = campo.value;
+    // mailformat = Char set <aA-zZ-0-9>@<aA-zZ-0-9>.<aA-zZ-0-9>.<aA-zZ-0-9> (domain infinite ammount of subdomains ".")
+    // Regex taken from (http://regexlib.com/REDetails.aspx?regexp_id=5011)
+    const mailformat = /^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z0-9.-]+$/i;
     // console.log(email.indexOf("@")); - Donde se encuentra el @ 
     // Si no hay un @ en el campo del email, la validacion devolverá -1. Si hay un @ en el texto, devuelve su posición en el input
-    if(email.indexOf("@") != -1){
-         // Cambiar el color del borde inferior a verde
-         campo.style.borderBottomColor = "green";
-         // Remover la clase "error" del elemento
-         campo.classList.remove("error");
+    // console.log(email.indexOf("."));
+    if (email.match(mailformat)) {
+        aprobarCampo(true, campo);
+    } else {
+        aprobarCampo(false, campo);
+    }
+}
+
+// Modificar los atributos del campo segun la orden enviada
+function aprobarCampo(orden, campo) {
+    // Si la orden es true
+    if (orden) {
+        // Cambiar el color del borde inferior a verde
+        campo.style.borderBottomColor = "green";
+        // Remover la clase "error" del elemento
+        campo.classList.remove("error");
     } else {
         // Cambiar el color del borde inferior a rojo
         campo.style.borderBottomColor = "red";
